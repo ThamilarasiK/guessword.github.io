2ebb0f590c5c3bec9a027fda6ad12e74f41fc249
Thamilarasik <164861330+ThamilarasiK@users.noreply.github.com>
2024-09-19 12:44:32 +0530

    Update words.js

--- words.js
+++ words.js
@@ -11,10 +11,7 @@ const wordList = [
         word: "aim",
         hint: "a purpose or intention"
     },
-    {
-        word: "venus",
-        hint: "planet of our solar system"
-    },
+   
     {
         word: "gold",
         hint: "a yellow precious metal"
@@ -23,20 +20,7 @@ const wordList = [
         word: "meesho",
         hint: "online shopping site"
     },
-    
-    {
-        word: "coding",
-        hint: "related to programming"
-    },
 
-    {
-        word: "mental",
-        hint: "related to the mind"
-    },
-    {
-        word: "map",
-        hint: "diagram represent of an area"
-    },
 
     {
         word: "hockey",
@@ -75,10 +59,7 @@ const wordList = [
         word: "google",
         hint: "famous search engine"
     },
-    {
-        word: "venice",
-        hint: "famous city of waters"
-    },
+
     {
         word: "excel",
         hint: "microsoft product for windows"
@@ -87,30 +68,12 @@ const wordList = [
         word: "mysql",
         hint: "a relational database system"
     },
-    {
-        word: "nepal",
-        hint: "developing country name"
-    },
-    {
-        word: "flute",
-        hint: "a musical instrument"
-    },
-    {
-        word: "crypto",
-        hint: "related to cryptocurrency"
-    },
-    {
-        word: "tesla",
-        hint: "unit of magnetic flux density"
-    },
+   
     {
         word: "mars",
         hint: "planet of our solar system"
     },
-    {
-        word: "proxy",
-        hint: "related to server application"
-    },
+   
     {
         word: "email",
         hint: "related to exchanging message"
@@ -119,10 +82,7 @@ const wordList = [
         word: "html",
         hint: "markup language for the web"
     },
-    {
-        word: "air",
-        hint: "related to a gas"
-    },
+   
     {
         word: "idea",
         hint: "a thought or suggestion"
@@ -153,13 +113,9 @@ const wordList = [
         hint: "psychological thriller film"
     },
     {
-        word: "dubai",
-        hint: "developed country name"
-    },
-    {
         word: "photo",
         hint: "representation of person or scene"
-    },,
+    },
     {
         word: "rain",
         hint: "related to a water"
